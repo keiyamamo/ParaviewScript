540d99f24ce9d0f619c04e9af9d6cfe0908393cb
Kei <withwhom0215@gmail.com>
2024-03-18 14:28:31 +0100

    improve time_average.oy

--- time_avrage.py
+++ time_avrage.py
@@ -5,30 +5,38 @@
 
 #### import the simple module from the paraview
 from paraview.simple import *
+from pathlib import Path
+import argparse
 #### disable automatic camera reset on 'Show'
 paraview.simple._DisableFirstRenderCameraReset()
 
-# create a new 'Xdmf3ReaderS'
-wSSxdmf = Xdmf3ReaderS(registrationName='WSS.xdmf', FileName=['/Users/keiyamamoto/Documents/VaSP/src/vasp/simulations/offset_stenosis_results/14/Hemodynamic_indices/WSS.xdmf'])
+def parse_command_line_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_path", type=str, required=True)
+    return parser.parse_args()
 
-# get animation scene
-animationScene1 = GetAnimationScene()
 
-# update animation scene based on data timesteps
-animationScene1.UpdateAnimationUsingDataTimeSteps()
+def compute_time_average(input_path):
+    # based on the input path, defined registration name and file name
+    file_name = Path(input_path).name
 
-UpdatePipeline(time=0.95, proxy=wSSxdmf)
+    # create a new 'Xdmf3ReaderS'
+    xdmf_file = Xdmf3ReaderS(registrationName=file_name, FileName=[input_path])
 
-# create a new 'Temporal Statistics'
-temporalStatistics1 = TemporalStatistics(registrationName='TemporalStatistics1', Input=wSSxdmf)
+    # create a new 'Temporal Statistics'
+    temporalStatistics1 = TemporalStatistics(registrationName='TemporalStatistics1', Input=xdmf_file)
 
-# Properties modified on temporalStatistics1
-temporalStatistics1.ComputeMinimum = 0
-temporalStatistics1.ComputeMaximum = 0
-temporalStatistics1.ComputeStandardDeviation = 0
+    # Properties modified on temporalStatistics1
+    temporalStatistics1.ComputeMinimum = 0
+    temporalStatistics1.ComputeMaximum = 0
+    temporalStatistics1.ComputeStandardDeviation = 0
+    output_path = input_path.replace('.xdmf', '_time_average.vtu')
+    # save data
+    SaveData(output_path, proxy=temporalStatistics1, DataMode='Ascii')
 
-UpdatePipeline(time=0.95, proxy=temporalStatistics1)
+def main():
+    args = parse_command_line_args()
+    compute_time_average(args.input_path)
 
-# save data
-SaveData('/Users/keiyamamoto/Documents/VaSP/src/vasp/simulations/offset_stenosis_results/14/Hemodynamic_indices/time_averaged_wss.vtu', proxy=temporalStatistics1, PointDataArrays=['WSS_average'],
-    DataMode='Ascii')
+if __name__ == "__main__":
+    main()
